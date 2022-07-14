bb680af008171d33b50bcbe43d3c351568b7e2a9
sadiki <76570335+sadikir@users.noreply.github.com>
2022-07-14 23:28:06 +0000

    edited the intro section

--- src/components/introSection/Intro.jsx
+++ src/components/introSection/Intro.jsx
@@ -6,7 +6,8 @@ const Intro=()=>{
       <div className="box1">
         <h1>We help immigrants to </h1>
         <h2> sponsor their relatives</h2>
-        <h1>By offering reliable financial </h1>
+        <h1>By offering reliable  </h1>
+        <h1>financial</h1>
         <h2>and legal support</h2>
         <p>#1 Platform for family sponsorship</p>
         <button>Join us</button>
